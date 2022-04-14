5dbe7aa55907e317e16468cb5262c1fce7a1bf9b
donaiski <donaiski@hotmail.com>
2022-04-14 16:22:54 -0300

    javascript

---
+++ js/main.js
@@ -0,0 +1,103 @@
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML * "<b>Obrigado por clicar</b>";
+function redirecionar(){
+    window.open("http://google.com"/);
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse"
+    
+     /*
+    alert("trocar texto")
+}
+}
+  /*
+    window.location.href = "http://google.com/";
+}
+    /*
+    console.log(document.getElementById("agradecimento"))
+  
+/*  
+    alert("Obrigado por clicar");
+}
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+/*
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+*/
+/*
+function validaIdade(idade){
+    var validar;
+    if (idade=18){
+        validar = true
+    } else{
+        validar = false
+    }
+    return validar;
+}
+var idade = prompt("Qual sua idade?");
+console.log(validadIdade(idade));
+
+
+/*
+var d = new Date();
+alert(d.getDate);
+alert(d.getFullYear);
+alert(d.getMinutes);
+*/
+
+/*
+var count;
+for (count=0; count<=5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while (count <=5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18){   
+    alert("maior de idade");
+}else{
+    alert("menor de idade")
+};
+*/
+
+/*
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(fruta[1].nome);
+*/
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta, nome;)
+alert(fruta, cor);
+*/
+
+/*
+var lista = ["maça", "pera", "laranja"]
+lista. push("uva");
+lista.pop(2);
+*/
+
+/*
+var nome = "Alysson Donaiski ";
+var idade = 41s;
+var frase = "País é o melhor time do mundo";
+alert("Bem vindo " + nome + idade + " anos");
+console.log(frase.replace("País", "Brasil"));
+alert(frase.replace("País", "Brasil"));
+*/
